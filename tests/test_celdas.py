9406092a9188d843945d93da2444ac6afe9e7475
Celeste <celesteyuliana@gmail.com>
2020-05-25 15:48:38 -0300

    Validar que los números de las celdas no se repitan

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -35,3 +35,21 @@ def test_contar_celdas_3():
         if( x == 1 or y ==1 or z == 1 ):
             i += 1
     assert i == 9
+
+def test_celdas_repetidas():
+    mi_carton = (
+        (1,2,3,15,16,4,5,6,17),
+        (18,19,10,20,21,22,23,7,24),
+        (8,25,26,27,9,28,12,13,14),
+    )
+    for fila in range(0, 3):
+        for columna in range(0, 9):
+            celda = mi_carton[fila][columna]
+            aux = columna + 1
+            if columna == 9:
+                aux = 0
+            for d_fila in range (fila, 3):
+                for d_columna in range (aux, 9):
+                    d_celda = mi_carton[d_fila][d_columna]
+                    assert celda != d_celda
+                aux = 0
